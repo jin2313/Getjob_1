30f39c3593fcc91a05ef34147f310e3afd7703cf
MinseoPark <kjms0820@gmail.com>
2022-05-25 02:08:28 +0900

    Add Eye Tracking 2

--- interviewapp/views.py
+++ interviewapp/views.py
@@ -8,7 +8,7 @@ from interviewapp.models import Question, Result
 
 import os
 import speech_recognition as sr
-import cv2   # cv2 대신 opencv-python 패키지 설치해줘야함!!
+import cv2
 import numpy as np
 
 
@@ -17,9 +17,10 @@ def QuestionView(request):
     if request.method == 'GET':
         corp_name = request.GET.get('corp', None)
         dept_name = request.GET.get('dept', None)
-        quest_num = request.GET.get('question', None)
-        next_num = str(int(quest_num) + 1)
-        question = Question.objects.filter(quest_id=quest_num)
+        quest_id = request.GET.get('question', None)
+        next_id = str(int(quest_id) + 1)
+        question = Question.objects.filter(quest_id=quest_id)
+        quest_level = str(question[0].level) if len(question) != 0 else '1'
         result = Result.objects.filter(user_id=request.user)
         if len(result) == 0:
             report_num = 1
@@ -31,42 +32,41 @@ def QuestionView(request):
             'question': question,
             'corp_name': corp_name,
             'dept_name': dept_name,
-            'next_num': next_num,
-            'quest_id': quest_num,
+            'next_id': next_id,
+            'quest_id': quest_id,
+            'quest_level': quest_level,
             'report_num': report_num
         }
-        if (quest_num == '4') or (quest_num == '5'):
+        if (quest_level == '3'):
             return render(request, 'interviewapp/tendency.html', context)
         else:
             return render(request, 'interviewapp/question.html', context)
 
 
-def TestView(request):
-    if request.method == 'GET':
-        video = request.GET.get('video', None);
-        context = {'video': video}
-        return render(request, 'interviewapp/test.html', context)
-    else:
-        file = request.FILES['file']
-        fname = file.name
-        if file.name[-4:] == 'webm':
+def ResultView(request):
+    if request.method == 'POST':
+        if request.POST['quest_level'] != '3':
+            file = request.FILES['file']
+            fname = file.name
             fs = FileSystemStorage(location='media/webm/')
             filename = fs.save(fname, file)
-            new_fname = f"{request.user}_{request.POST['quest_id']}"
+            new_fname = f"{request.user}_{request.POST['report_num']}_{request.POST['quest_id']}"
             os.system(f"ffmpeg -y -i media/webm/{filename} media/mp4/{new_fname}.mp4")
             os.system(f"ffmpeg -y -i media/webm/{filename} media/wav/{new_fname}.wav")
-            # eye = run_eyetrack(f'media/mp4/{new_fname}.mp4')
+            os.remove(f"media/webm/{filename}")
+            total, good = run_eyetrack(f'media/mp4/{new_fname}.mp4')
             text = run_stt(f'media/wav/{new_fname}.wav')
             user = User.objects.get(username=request.user.username)
             quest = Question.objects.get(quest_id=request.POST['quest_id'])
             Result.objects.create(user_id=user, report_num=request.POST['report_num'], quest_id=quest, result_stt=text)
-        else:
-            fs = FileSystemStorage(location='media')
-            filename = fs.save(fname, file)
+            tendency = request.POST.getlist('tendency')
+            str_tendency = ', '.join(tendency)
+            user = User.objects.get(username=request.user.username)
+            quest = Question.objects.get(quest_id=request.POST['quest_id'])
+            Result.objects.create(user_id=user, report_num=request.POST['report_num'], quest_id=quest,
+                                  result_add=str_tendency)
+
         return render(request, 'interviewapp/question.html')
-
-
-# def TendencyView(request):
 
 
 def ThresView(request):
@@ -148,8 +148,8 @@ def nothing(x):
 
 
 def run_eyetrack(file_path):
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
     detector_params = cv2.SimpleBlobDetector_Params()
     detector_params.filterByArea = True
@@ -157,29 +157,33 @@ def run_eyetrack(file_path):
     detector = cv2.SimpleBlobDetector_create(detector_params)
 
     cap = cv2.VideoCapture(file_path)
-    # cv2.namedWindow('image')
     keypoint_list = []
-    # cv2.createTrackbar('threshold', 'image', 0, 255, nothing)
+    good = 0
 
-    while (cap.isOpened()):
-        _, frame = cap.read()
-        face_frame = detect_faces(frame, face_cascade)
-        if face_frame is not None:
-            eyes = detect_eyes(face_frame, eye_cascade)
-            for eye in eyes:
-                if eye is not None:
-                    # threshold = cv2.getTrackbarPos('threshold', 'image')
-                    threshold = 123   # 전달 받은 threshold 값 넣어주기
-                    eye = cut_eyebrows(eye)
-                    # keypoints = blob_process(eye, threshold, detector)
-                    keypoints = blob_process(eye, threshold, detector)
-                    keypoint_list.append(keypoints)
-                    eye = cv2.drawKeypoints(eye, keypoints, eye, (0, 0, 255),
-                                            cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-        # cv2.imshow('image', frame)
-        # if cv2.waitKey(1) & 0xFF == ord('q'):
-        #    break
+    while True:
+        ret, frame = cap.read()
+        if ret:
+            face_frame = detect_faces(frame, face_cascade)
+            if face_frame is not None:
+                eyes = detect_eyes(face_frame, eye_cascade)
+                for eye in eyes:
+                    if eye is not None:
+                        # threshold = cv2.getTrackbarPos('threshold', 'image')
+                        threshold = 120   # 전달 받은 threshold 값 넣어주기(일단 내 방 조명에 맞춰 설정한거임)
+                        eye = cut_eyebrows(eye)
+                        keypoints = blob_process(eye, threshold, detector)
+
+                        if len(keypoints) != 0:
+                            good += 1
+                        keypoint_list.append(keypoints)
+
+                        eye = cv2.drawKeypoints(eye, keypoints, eye, (0, 0, 255),
+                                                cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        else:
+            break
+
+    good += 20   # 사람은 1분에 평균 20회 정도 눈을 깜박인다고 함
+    # print(len(keypoint_list), good)
     cap.release()
-    # cv2.destroyAllWindows()
 
-    return keypoint_list
+    return len(keypoint_list), good
